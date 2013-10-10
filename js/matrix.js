4d19f7f3ad6aa9703e210e57ee7d33d823461ccb
koroshiya <korocresent@gmail.com>
2013-10-10 14:58:22 +1100

    Replaced table with divs; not as human-readable, but significantly faster

--- js/matrix.js
+++ js/matrix.js
@@ -11,37 +11,44 @@
  *
  */
 
-    function digiRain(width, height, freq, speed, fontcolor, fontsize, bgcolor) {
-
-        var time = new Date().getTime();
+     function buildInit(time){ //moved out for proper garbage collection
         var digirain = document.getElementById("DigiRain");
 
-        digirain.style.color = fontcolor;
+        digirain.style.color = '#20F54A';
         digirain.style.visibility = 'hidden';
 
         // characters used in rain
         //var rain = ['Â','µ','Â','¬','Â','¿','Ã','ƒ','Æ','˜','#','$','&','%','Ç','È','º','Ê','„','Ï','´','Ð','ч','а','щ','а','х','ю','г','а','ж','и','л','б','ы','ц','и','т','р','у','с','Д','а','н','о','ф','а','л','ь','ш','и','в','ы','й','э','к','з','е','м','п','л','я','р'];
 
-        var appendText = '<table>';
+        var appendText = '<div>';
         var i = -1;
         var j;
+        var cox;
+        var coy;
         while (++i < height) {
-
-            appendText += '<tr>';
+            cox = i*14;
 
             var rand;
             j = -1;
-            while (++j < width) {
-                rand = String.fromCharCode(12448 + Math.random() * 96);
-                appendText += ('<td id="digiRain' + time + '-' + i +'-' + j + '" style="visibility: hidden">' + rand + '</td>');
-            }
 
-            appendText += ('</tr>');
+            while (++j < width) {
+                coy = j*16;
+                rand = String.fromCharCode(12448 + Math.random() * 96);
+
+                appendText += ('<div id="digiRain' + time + '-' + i +'-' + j + 
+                    '" style="visibility:hidden;left:'+coy+'px;top:'+cox+'px;width:14px;position:absolute;">' + rand + '</div>');
+            }
         }
-        appendText += '</table>';
+        appendText += '</div>';
 
         digirain.insertAdjacentHTML('beforeend', appendText);
         digirain.style.visibility = 'visible';
+    }
+
+    function digiRain(width, height, freq, speed, fontcolor, fontsize, bgcolor) {
+
+        var time = new Date().getTime();
+        buildInit(time);
 
         var rainDrops = [];
 
@@ -71,7 +78,7 @@
 // Raindrop class
 function digiRainDroplet(row, col, windowheight, time) {
     
-    var rainheight = 10;
+    var rainheight = 15;
     var dead = false;
     var sp = 0.4 + Math.random();
     var cur = 0;
