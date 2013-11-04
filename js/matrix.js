42231f1e232a230ede9d722be853f77b7945ef94
koroshiya <korocresent@gmail.com>
2013-11-04 11:36:02 +1100

    Various performance improvements

--- js/matrix.js
+++ js/matrix.js
@@ -8,56 +8,83 @@
  * Based on Digital Rain jQuery plugin by James Seibel in 2010.
  * This version removes unnecessary overhead and dramatically improves performance.
  * Usage examples and the like can be found in the source code of the original plugin.
+ * 
+ * Contrary to the name, this script no longer requires jQuery.
+ * The removal of jQuery was one of many aforementioned performance/overhead improvements.
  *
  */
 
-     function buildInit(time){ //moved out for proper garbage collection
+    var width = ~~(window.innerWidth / 16);
+    var height = ~~(window.innerHeight / 7);
+
+    var shadow = true; //set to true to enable fade effect for rain. May cause slowdown.
+    var gleam = true; //set to true for gleam effect on first drop in rain stream.
+    var GLOBAL_DIV_ARRAY = new Array();
+
+    function buildInit(){ //moved out for proper garbage collection
         var digirain = document.getElementById("DigiRain");
 
         digirain.style.color = '#20F54A';
         digirain.style.visibility = 'hidden';
 
-        // characters used in rain
-        //var rain = ['Â','µ','Â','¬','Â','¿','Ã','ƒ','Æ','˜','#','$','&','%','Ç','È','º','Ê','„','Ï','´','Ð','ч','а','щ','а','х','ю','г','а','ж','и','л','б','ы','ц','и','т','р','у','с','Д','а','н','о','ф','а','л','ь','ш','и','в','ы','й','э','к','з','е','м','п','л','я','р'];
-
         var appendText = '<div>';
         var i = -1;
-        var j;
-        var cox;
-        var coy;
-        while (++i < height) {
-            cox = i*14;
+        var j = -1;
+        GLOBAL_DIV_ARRAY = new Array(width);
+        while (++j <= width) {
+            GLOBAL_DIV_ARRAY[j] = new Array(height);
+        }
+        while (++i <= height) {
 
             var rand;
             j = -1;
 
-            while (++j < width) {
-                coy = j*16;
+            while (++j <= width) {
                 rand = String.fromCharCode(12448 + Math.random() * 96);
 
-                appendText += ('<div id="digiRain' + time + '-' + i +'-' + j + 
-                    '" style="visibility:hidden;left:'+coy+'px;top:'+cox+'px;width:14px;position:absolute;">' + rand + '</div>');
+                appendText += ('<div id="digiRain' + i +'-' + j + 
+                    '" class="hidden" style="left:'+(j*16)+'px;top:'+(i*14)+'px;width:14px;position:absolute;">' + rand + '</div>');
             }
         }
-        appendText += '</div>';
+        appendText += '</div><style type="text/css" media="screen">'+
+        '.hidden{visibility: hidden;}'+
+        '.visible{visibility: visible;}'+
+        '.head{visibility: visible; color:#DAFFDA;}'+
+        '.nhead{color:#0F0;}'+
+        '.opa1{opacity:0.1;}'+
+        '.opa2{opacity:0.2;}'+
+        '.opa3{opacity:0.3;}'+
+        '.opa4{opacity:0.4;}'+
+        '.opa5{color:#0F0; opacity:0.5;}'+
+        '.opa6{color:#0F0; opacity:0.6;}'+
+        '.opa7{color:#0F0; opacity:0.7;}'+
+        '.opa8{color:#0F0; opacity:0.8;}'+
+        '.opa9{color:#0F0; opacity:0.9;}'+
+    '</style>';
 
         digirain.insertAdjacentHTML('beforeend', appendText);
+        j = -1;
+        while(++j <= width){
+            i = -1;
+            var glob_row = GLOBAL_DIV_ARRAY[j];
+            while(++i <= height){
+                glob_row[i] = document.getElementById("digiRain" + i + '-' + j);
+            }
+        }
         digirain.style.visibility = 'visible';
     }
 
-    function digiRain(width, height, freq, speed, fontcolor, fontsize, bgcolor) {
+    function digiRain(width, height, rainheight, freq, speed) {
 
-        var time = new Date().getTime();
-        buildInit(time);
+        buildInit();
 
         var rainDrops = [];
 
         this.makeItRain = function() {
 
-            var rand = Math.random();
-            if (rand < freq) {
-                var c = Math.floor(rand * width);
-                var droplet = new digiRainDroplet(0, c, height, time);
+            if (Math.random() < freq) {
+                var c = Math.floor(Math.random() * width);
+                var droplet = new digiRainDroplet(c, rainheight, height);
                 rainDrops.push(droplet);
             }
             var rainTemp = [];
@@ -76,14 +103,17 @@
     };
 
 // Raindrop class
-function digiRainDroplet(row, col, windowheight, time) {
+function digiRainDroplet(col, rh, windowheight) {
     
-    var rainheight = 15;
+    var rainheight = Math.floor(Math.random() * rh) + 10;
+    var rhdiv = 10 / rainheight;
     var dead = false;
-    var sp = 0.4 + Math.random();
+    var sp = 0.6 + Math.random();
     var cur = 0;
     var DROPLET_SPEED = 0.8;
-	
+    var glob_row = GLOBAL_DIV_ARRAY[col];
+    var row = -1;
+
     // animation of falling
     this.fall = function() {
         cur += sp;
@@ -91,18 +121,26 @@ function digiRainDroplet(row, col, windowheight, time) {
             cur -= DROPLET_SPEED;
             row++;
             if (row < windowheight){
-                document.getElementById("digiRain" + time + '-' + (row) + '-' + col).style.visibility = 'visible';
+                if (gleam){
+                    glob_row[row].className = 'head';
+                }else{
+                    glob_row[row].className = 'visible';
+                }
+                if (!shadow){
+                    glob_row[row-1].className = 'nhead';
+                }
                 var i = 0;
-                while(++i < rainheight && row >= i){
-                    document.getElementById("digiRain" + time + '-' + (row-i) + '-' + col).style.opacity = (1 - i/rainheight);
-                    //IE: .style.filter = "alpha(opacity="+(1 - i/rainheight)+")";
+                if (shadow){
+                    while(++i < rainheight && row >= i){
+                        glob_row[row-i].className = 'opa'+(10 - Math.round(i*rhdiv));
+                    }
                 }
             }
             if ((row - rainheight) >= 0) {
                 if ((row - rainheight) > windowheight) {
                     dead = true;
                 }else {
-                    document.getElementById("digiRain" + time + '-' + (row - rainheight) + '-' + col).style.visibility = 'hidden';
+                    glob_row[row - rainheight].className = 'hidden';
                 }
             }
         }
@@ -110,9 +148,4 @@ function digiRainDroplet(row, col, windowheight, time) {
     };
 }
 
-
-    var fontSize = 12;
-    var fontSizeScale = fontSize + 2;
-    var width = window.screen.width / fontSizeScale * 2;
-    var height = window.screen.height / fontSizeScale;
-    digiRain(width, height, 0.5, 100, '#20F54A', fontSize, '#000000');
+digiRain(width, height, 10, 0.5, 80);
